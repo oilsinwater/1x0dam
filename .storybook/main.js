3987e5a1d134e52218866b3513493953da588a08
Phil Drummond <oilsinwater@gmail.com>
2022-06-28 21:53:35 -0700

    Installs addons for storybook next & source.

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,9 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-storysource",
+    "storybook-addon-next"
   ],
   "framework": "@storybook/react",
   "core": {
