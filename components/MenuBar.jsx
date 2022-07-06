467a1038f7397f1bf635cffb2c0158a1e00365e4
Phil Drummond <oilsinwater@gmail.com>
2022-07-05 17:16:50 -0700

    Starts MenuBar component.

---
+++ components/MenuBar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { MenuButton } from "./MenuButton";
+import { UtilityButtons } from "./UtilityButtons";
+
+export const MenuBar = ({ primary }) => {
+  return (
+    <div className='MenuBar'>
+      <MenuButton />
+      <UtilityButtons />
+      <style jsx>{`
+        .MenuBar {
+          width: 43px;
+          height: 1024px;
+          flex-grow: 0;
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-start;
+          align-items: flex-end;
+          padding: 0;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+MenuBar.PropTypes = {};
+
+MenuBar.defaultProps = {};
---
+++ components/MenuBar.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { MenuBar } from './MenuBar'
+
+export default {
+    title: 'Menu/Bar',
+    component: MenuBar,
+}
+
+export const Primary = <MenuBar primary></MenuBar>
