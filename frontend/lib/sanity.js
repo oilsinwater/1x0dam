adc0a758fdf8dddce145463d5002c1ad16f8e20f
Phil Drummond <oilsinwater@gmail.com>
2022-07-18 14:40:25 -0700

    Installs sanity client.

---
+++ frontend/lib/sanity.js
@@ -0,0 +1,11 @@
+import sanityClient from '@sanity/client';
+
+const options = {
+    dataset: process.env.SANITY_DATASET_NAME,
+    projectId: process.env.SANITY_PROJECT_ID,
+    useCdn: process.env.NODE_ENV === 'production'
+    // useCdn === true gives fast response using cached data
+    // useCdn === false, gives you a slower response with latest data
+}
+
+export default sanityClient(options);
